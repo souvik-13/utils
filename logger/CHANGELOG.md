655dffb86078a3c9cd9dbb8d73581aaa03963a93
Souvik <souvikk431@gmail.com>
2025-08-30 22:47:56 +0530

    added versioning info for logger

---
+++ logger/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## v3.0.0 - 2024-08-30
+
+- Added support for setting output file
---
+++ logger/version.go
@@ -0,0 +1,4 @@
+package logger
+
+// Version is the current version of the logger package
+const Version = "3.0.0"
